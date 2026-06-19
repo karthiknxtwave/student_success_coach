417a6dddf515dbd411bfa6b3d6717d4da45b6412
Karthik <karthik.gnaik@nxtwave.co.in>
2026-06-19 10:56:25 +0530

    adding path in main file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
 import streamlit as st
 from agents.conversation_agent import ConversationAgent
 from sheets.client import fetch_all_students
